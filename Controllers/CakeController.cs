f2703dbe576da83634395892c2a0771158d93c26
jcasaliglla <jcasaliglla.biometrika@gmail.com>
2022-05-03 18:43:08 -0500

    Se agrega controller para manejo API de pasteles

---
+++ Controllers/CakeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestApi.Controllers
+{
+    public class CakeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
